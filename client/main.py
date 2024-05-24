9479d9987451d64997cddfcfc63b461b4ca89fc5
Usling <frozenjaws@live.dk>
2024-05-24 15:47:15 +0200

    echo server

--- client/main.py
+++ client/main.py
@@ -1,1 +1,21 @@
-import httpx
+import socket
+import logging
+
+
+HOST = "127.0.0.1"
+PORT = 35677
+
+logger = logging.getLogger(__name__)
+
+
+def start_client():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"Hello, World!")
+        data = s.recv(1024)
+    logger.info(f"Client got back: {data}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    start_client()
--- server/main.py
+++ server/main.py
@@ -1,1 +1,27 @@
-import FastAPi
+import socket
+import logging
+
+HOST = "127.0.0.1"
+PORT = 35677
+
+logger = logging.getLogger(__name__)
+
+
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+
+        conn, addr = s.accept()
+        with conn:
+            logger.info("connected")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    start_server()
